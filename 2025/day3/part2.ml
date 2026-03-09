716d2934b18247a8f95335c2fdb8914dd1b808a7
张睿 Zhang Rui <zrui16@hotmail.com>
2026-03-09 10:50:54 -0700

    Update part2.ml

--- 2025/day3/part2.ml
+++ 2025/day3/part2.ml
@@ -1,22 +1,20 @@
 let max_joltage n line =
   let len = String.length line in
   assert (len >= n);
-  (* dp[i][j] := maximum joltage of length i in config[j..len-1] *)
-  let dp = Array.init_matrix (n + 1) len (fun _ _ -> "") in
+  (* After i-th iteration:
+    dp.(j) is maximum joltage of length i in line.[j..len-1] *)
+  let dp = Array.init len (Fun.const "") in
   for i = 1 to n do
-    dp.(i).(len - i) <- String.sub line (len - i) i;
+    let prev = ref dp.(len - i) in
+    dp.(len - i) <- String.sub line (len - i) i;
     for j = len - i - 1 downto 0 do
-      let seq1 = dp.(i).(j + 1) in
-      let seq2 =
-        let b = Bytes.create i in
-        Bytes.set b 0 line.[j];
-        Bytes.blit_string dp.(i - 1).(j + 1) 0 b 1 (i - 1);
-        Bytes.unsafe_to_string b
-      in
-      dp.(i).(j) <- max seq1 seq2
+      let seq1 = dp.(j + 1) in
+      let seq2 = String.make 1 line.[j] ^ !prev in
+      prev := dp.(j);
+      dp.(j) <- max seq1 seq2
     done
   done;
-  int_of_string dp.(n).(0)
+  int_of_string dp.(0)
 
 let () =
   In_channel.(input_lines stdin)
